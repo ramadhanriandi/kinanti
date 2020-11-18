f7a2e3c303da85ef2836f7e12b8dd7ac46fff556
ramadhanriandi <izurbudianto@gmail.com>
2020-11-18 10:28:21 +0700

    change request body of get reservation API

--- backend/controllers/reservation.controller.js
+++ backend/controllers/reservation.controller.js
@@ -1,3 +1,4 @@
+Datetime = require('../utils/datetime');
 Doctor = require('../models/doctor.model');
 Reservation = require('../models/reservation.model');
 
@@ -33,19 +34,44 @@ exports.add = function (req, res) {
 
 exports.find = function (req, res) {
   if (req.query.user_id) {
-    Reservation.find({ user_id: req.query.user_id }, function (err, reservation) {
-      if (err)
+    Doctor.get(function (doctorErr, doctor) {
+      if (doctorErr)
         res.json({
           status: "error",
-          message: err,
+          message: doctorErr,
         });
 
-      res.json({
-        status: "success",
-        message: "Search reservations result",
-        data: reservation,
+      Reservation.find({ user_id: req.query.user_id }, function (reservationErr, reservation) {
+        if (reservationErr)
+          res.json({
+            status: "error",
+            message: reservationErr,
+          });
+
+        let data = "List Semua Reservasi:\n";
+
+        for (i = 0; i < reservation.length; i++) {
+          let reservationDateTime = new Date(reservation[i].datetime);
+          data += `\n[${i + 1}]\n`;
+          data += `Tanggal: ${reservationDateTime.getDate()} ${Datetime.getMonthName(reservationDateTime.getMonth())} ${reservationDateTime.getFullYear()}\n`;
+          data += `Jam: ${reservationDateTime.getHours()}:${reservationDateTime.getMinutes() < 10 ? '0' : ''}${reservationDateTime.getMinutes()}\n`;
+
+          for (j = 0; j < doctor.length; j++) {
+            if (JSON.stringify(reservation[i].doctor_id) === JSON.stringify(doctor[j]._id)) {
+              data += `Dokter: ${doctor[j].name}\n`;
+              data += `Alamat: ${doctor[j].address}\n`;
+            }
+          }
+        }
+
+        res.json({
+          status: "success",
+          message: "Search reservations result",
+          data: data,
+        });
       });
     });
+
   } else if (req.query.date) {
     let tomorrow = new Date(req.query.date);
     tomorrow.setDate(tomorrow.getDate() + 1);
