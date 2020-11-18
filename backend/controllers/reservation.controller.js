01cc3c165eb93a1bf52b5aa4af45da0738d7bda7
ramadhanriandi <izurbudianto@gmail.com>
2020-11-18 09:37:18 +0700

    change request body of post reservation API

--- backend/controllers/reservation.controller.js
+++ backend/controllers/reservation.controller.js
@@ -1,22 +1,33 @@
+Doctor = require('../models/doctor.model');
 Reservation = require('../models/reservation.model');
 
 exports.add = function (req, res) {
   let reservation = new Reservation();
 
-  reservation.user_id = req.body.user_id;
-  reservation.name = req.body.name;
-  reservation.datetime = req.body.datetime;
-  reservation.doctor_id = req.body.doctor_id;
-
-  reservation.save(function (err) {
-    if (err)
-      res.json(err);
-    else {
+  Doctor.find({ name: req.body.doctor_name }, function (doctorErr, doctor) {
+    if (doctorErr)
       res.json({
-        message: "New Reservation Added!",
-        data: reservation,
+        status: "error",
+        message: doctorErr,
       });
-    }
+    
+    let requestedDate = new Date(req.body.date);
+    
+    reservation.user_id = req.body.user_id;
+    reservation.name = req.body.name;
+    reservation.datetime = `${requestedDate.getFullYear()}-${requestedDate.getMonth() + 1}-${requestedDate.getDate()}T${req.body.time}:00.000Z`;
+    reservation.doctor_id = doctor[0]._id;
+
+    reservation.save(function (reservationErr) {
+      if (reservationErr)
+        res.json(reservationErr);
+      else {
+        res.json({
+          message: "New Reservation Added!",
+          data: reservation,
+        });
+      }
+    });
   });
 };
 
